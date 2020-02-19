7a329342833e92b3e42bf4e08ec0815f0088dee9
Samuel Ruiz <samuel.ruiz@aktios.com>
2020-02-19 18:31:45 +0100

    mas ejemplos de pipes slice, number, percent, currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   nombre: string = 'Capitan America';
-  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI: number = Math.PI;
+
+  porcentaje: number = 0.234;
+
+  salario: number = 1234.5;
+
 }
 
