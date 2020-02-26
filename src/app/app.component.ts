253b164fd873774cb212f3d834aedcc88854758d
Samuel Ruiz <samuel.ruiz@aktios.com>
2020-02-26 18:35:31 +0100

    capitalizado pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
 
   nombre: string = 'Capitan America';
 
+  nombre2: string = 'saMUEL rUiz de la ROSA';
+
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   PI: number = Math.PI;
@@ -16,6 +18,29 @@ export class AppComponent {
   porcentaje: number = 0.234;
 
   salario: number = 1234.5;
+
+  fecha: Date = new Date();
+
+  idioma: string = 'en';
+
+  videoURL: string = 'https://www.youtube.com/embed/wXhTHyIgQ_U'
+
+
+  valorPromesa = new Promise((resolve)=>{
+    setTimeout(function(){
+      resolve("¡LLegaron los datos!"); // ¡Todo salió bien!
+    }, 4500);
+  })
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion:{
+      calle: 'Primera',
+      casa: 20
+    }
+  }
 
 }
 
